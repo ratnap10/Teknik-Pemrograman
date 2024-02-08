15f67341c8a3e4d9a55d3ef0a111571fb19e88e2
Ratna <150262952+ratnap10@users.noreply.github.com>
2024-02-08 19:37:55 +0700

    Update AnalisisSatu.java

--- Praktikum 1/AnalisisSatu.java
+++ Praktikum 1/AnalisisSatu.java
@@ -7,3 +7,6 @@ public class AnalisisSatu {
         System.out.println("Hasil 1 " +hasil);
     }
 }
+
+//outputnya Hasil 1 -125 karena byte hanya bisa menampung nilai -128 sampai 127, sedangkan 125+6 = 131, maka 131-256 adalah -125.
+//referensi: https://stackoverflow.com/questions/2458495/how-are-integers-cast-to-bytes-in-java
