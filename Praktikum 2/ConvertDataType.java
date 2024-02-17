d76310ff18180220d060624a85655db2a579dc86
Ratna <150262952+ratnap10@users.noreply.github.com>
2024-02-17 09:25:22 +0700

    Update and rename ConvertDataType.java to Praktikum 2/ConvertDataType.java

--- Praktikum 2/ConvertDataType.java
+++ Praktikum 2/ConvertDataType.java
@@ -10,3 +10,8 @@ class ConvertDataType {
         System.out.println(b);
     }
 }
+
+/*
+Output dari b adalah 10. Dari variabel d yang bertipe double, dikonversi ke float, lalu pada methodOne dikonversi lagi ke int
+yang nantinya mengembalikan nilai bertipe short dan terakhir di ubah ke variabel b bertipe byte.
+*/
