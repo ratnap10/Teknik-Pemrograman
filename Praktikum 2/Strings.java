69fa5e6f12526fb1ca9f64050189600debaccab8
Ratna <150262952+ratnap10@users.noreply.github.com>
2024-02-17 09:27:01 +0700

    Update and rename Strings_W2.java to Praktikum 2/Strings.java

--- Praktikum 2/Strings.java
+++ Praktikum 2/Strings.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Strings_W2 {
     public static void main(String[] args) {
         Scanner ketik = new Scanner(System.in);
-        String A = ketik.nextLine();
+        String A = ketik.nextLine(); //inputan string
         String B = ketik.nextLine();
         
-        int jumhrf = A.length() + B.length();
-        String banding = A.compareTo(B) > 0 ? "Yes":"No";
-        String kptlA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        int jumhrf = A.length() + B.length(); //menghitung panjang char
+        String banding = A.compareTo(B) > 0 ? "Yes":"No"; //membandingkan kedua string
+        String kptlA = A.substring(0, 1).toUpperCase() + A.substring(1); //mengubah huruf pertama jadi kapital
         String kptlB = B.substring(0, 1).toUpperCase() + B.substring(1);
         
         System.out.println("\n" + jumhrf);
